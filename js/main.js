ecc351a4dbf20193ee295254f1f49f20bef7ef38
ZakharovAndrew <37038505+ZakharovAndrew@users.noreply.github.com>
2019-01-29 18:09:36 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 Tamagothci.initialize('Rabbit');
 console.log(Tamagothci);
 
+var time = window.setInterval(function () {
+	console.log('Timer');
+	if (Tamagothci.isAlive() === true) {
+		Tamagothci.step();
+		// update values
+		$("#healthLevel").html(Tamagothci.healthLevel);
+		$("#foodLevel").html(Tamagothci.foodLevel);
+		$("#happinessLevel").html(Tamagothci.happinessLevel);
+		$("#face").html(Tamagothci.states);
+	} else {
+		window.clearInterval(time);
+        $("#tamagothci").hide();
+        $("#info").html(Tamagothci.name + " is Dead.");
+	}
+}, 2000);
+
 $("#medicine").click (function () {
 	Tamagothci.medicine();
 	console.log(Tamagothci);
--- js/tamagotchi.js
+++ js/tamagotchi.js
@@ -11,19 +11,19 @@ var Tamagothci = {
 		this.foodLevel = this.maxFood;
 		this.happinessLevel = this.maxHappiness;		
 	},
-	
+
 	medicine: function () {
 		if (this.healthLevel < this.maxHealth) {
 			this.healthLevel++;
 		};
 	},
-	
+
 	feed: function() {
 		if (this.foodLevel < this.maxFood) {
 			this.foodLevel++;
 		}
 	},
-	
+
 	play: function () {
 		if (this.happinessLevel < this.maxHappiness) {
 			this.happinessLevel++;
@@ -33,10 +33,32 @@ var Tamagothci = {
 	goSleep: function () {
 		this.isSleeping = true;
 	},
+
+	isAlive: function () {
+        if (this.foodLevel > 0 && this.healthLevel > 0 ) {
+            return true;
+        } else {
+            return false;
+        }
+    },
 	
+	states: function () {
+        var score = (Tamagothci.healthLevel + Tamagothci.foodLevel + Tamagothci.happinessLevel)/3;
+		if (score > 7) {
+            return "(ಠ‿ಠ)";
+        } else if (score > 3) {
+            return "(ಠ_ಠ)";
+        } else {
+            return "(ಥ﹏ಥ)";
+        };
+    },
+
 	step: function () {
 		this.healthLevel--;
 		this.foodLevel--;
 		this.happinessLevel--;
+		if (this.happinessLevel < 0) {
+			this.happinessLevel = 0;
+		}
 	},
 }
